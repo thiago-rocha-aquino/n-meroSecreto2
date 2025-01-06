63f5766a6268a04d76f9cd3080fcb668d70e7e1a
Thiago Rocha de Aquino <91508287+thiago-rocha-aquino@users.noreply.github.com>
2025-01-06 15:19:37 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+
+function exibirTexto(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;  
+}
+exibirTexto('h1', 'Jogo do número secreto');
+exibirTexto('p', 'escolha um número entre 1 e 10');
+
+
+
+function verificarChute(){
+    console.log('o botão foi clicado');
+}
+ 
